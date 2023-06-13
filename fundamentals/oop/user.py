ca13b84f43c8828581e5427a0311f03a72370e57
Adrian Pena <adrianpena383@gmail.com>
2023-06-13 11:26:36 -0400

    chained methods for each user

--- fundamentals/oop/user.py
+++ fundamentals/oop/user.py
@@ -8,41 +8,32 @@ class User:
         self.gold_card_points = 0
 
     def display_info(self):
-        pass
         print(f'{self.first_name}\n{self.last_name}\n{self.email}\n{self.age}\n{self.is_rewards_member}\n{self.gold_card_points}')
+        return self
 
     def enroll(self):
-#        pass
         if self.is_rewards_member == True:
             print('already a rewards member')
         else:
             self.is_rewards_member = True
             self.gold_card_points = 200
-
+        return self
+    
     def spend_points(self, amount):
         if self.gold_card_points - amount <= 0:
             print('not enough points remaining')
         else:
             self.gold_card_points = self.gold_card_points - amount
-
+        return self
 
 person1 = User('John', 'Doe', 'JohnDoe@email.com', 30)
-person1.enroll()
 
 person2 = User('Jane', 'Smith', 'JaneSmith@email.com', 22)
-person2.enroll()
-person2.spend_points(50)
 
 person3 = User('Dude', 'Guy', 'DudeGuy@email.com', 40)
-person3.enroll()
-person3.spend_points(80)
 
+person1.display_info().enroll().display_info().enroll()
+person2.display_info().enroll().spend_points(50).display_info()
+person3.display_info().enroll().spend_points(80).spend_points(100).spend_points(40)
 
-person1.display_info()
-person2.display_info()
-person3.display_info()
-
-person1.enroll()
-person3.spend_points(100)
-person3.spend_points(40)
 
